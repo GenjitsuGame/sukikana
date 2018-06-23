273a7be3a1735e6542bef4cee043a99dd14db93e
GenjitsuGame <pascal.luttgens@opendatasoft.com>
2018-06-23 17:01:20 +0200

    update boto_utils

--- tasks/data/boto_utils.py
+++ tasks/data/boto_utils.py
@@ -14,7 +14,7 @@ def get_session():
         config.read(config_path)
 
     return boto3.session.Session(
-        aws_access_key_id=os.environ.get('SUKIKANA_AWS_ACCESS', config['AWS']['access_key']),
-        aws_secret_access_key=os.environ.get('SUKIKANA_AWS_SECRET_KEY', config['AWS']['secret_key']),
-        region_name=os.environ.get('SUKIKANA_AWS_REGION', config['AWS']['region'])
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID', config.get('AWS', 'access_key')),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY', config.get('AWS', 'secret_key')),
+        region_name=os.environ.get('SUKIKANA_AWS_REGION', config.get('AWS', 'region'))
     )
