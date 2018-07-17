54e3ed81f6588f7fe0e81763789d0db9e22025aa
Maau212 <mau.costier@gmail.com>
2018-07-17 17:45:29 +0200

    Create cosine_similarity.py

---
+++ cosine_similarity.py
@@ -0,0 +1,7 @@
+import sklearn.metrics as metrics
+import pandas as pd
+
+df = pd.read_csv("./song_factors.csv", delimiter=";")
+df = df.loc[:, df.columns != 'song_id']
+cosine = metrics.pairwise.cosine_similarity(df.T,df.T)
+cosine
